87dac85d8cca484c5d431849d06dd5b5a227f2ea
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2026-07-20 01:34:45 +0530

    added

--- Server/controllers/Authentication/LoginController.js
+++ Server/controllers/Authentication/LoginController.js
@@ -25,9 +25,9 @@ const Login = async (req, res) => {
 
             res.cookie("token", token, {
                 httpOnly: true,
-
-                sameSite: "strict",
-                maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days, matches JWT_EXPIRES_IN default
+                secure: true,
+                sameSite: "none",
+                maxAge: 7 * 24 * 60 * 60 * 1000,
             });
 
             return res.status(200).json({
--- Server/controllers/Authentication/SignupController.js
+++ Server/controllers/Authentication/SignupController.js
@@ -26,9 +26,9 @@ const SignupController = async (req, res) => {
 
             res.cookie("token", token, {
                 httpOnly: true,
-
-                sameSite: "strict",
-                maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days, matches JWT_EXPIRES_IN default
+                secure: true,
+                sameSite: "none",
+                maxAge: 7 * 24 * 60 * 60 * 1000,
             });
 
             return res.status(201).json({
